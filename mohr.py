c8af8cf3fd5d82e3f30deadb5daee3adce89441e
liani <liani@lychee.(none)>
2015-04-22 10:38:07 -0400

    first pass at 3d mohrs circle

--- mohr.py
+++ mohr.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import math
+import numpy as np
 
 def convert(sigxx, sigyy, tauxy, theta):
 	rad = math.radians(theta)
@@ -8,6 +9,29 @@ def convert(sigxx, sigyy, tauxy, theta):
 	tauxyp = -(sigxx-sigyy)*math.sin(2*rad)/2 + tauxy*math.cos(2*rad)
 
 	return [sigxxp, sigyyp, tauxyp]
+
+
+def plot3d(sigxx, sigyy, sigzz, tauxy, tauxz, tauyz):
+
+	plt.hold(True)
+	for i in range(360):
+		[sigxxp1, sigyyp1, tauxyp] = convert(sigxx, sigyy, tauxy, i)
+		[sigxxp2, sigzzp1, tauxzp] = convert(sigxx, sigzz, tauxz, i)
+		[sigyyp2, sigzzp2, tauyzp] = convert(sigyy, sigzz, tauyz, i)
+
+		plt.plot(sigxxp1, tauxyp, 'ro')
+		plt.plot(sigyyp1, -tauxyp, 'ro')
+
+		plt.plot(sigxxp2, tauxzp, 'bo')
+		plt.plot(sigzzp1, -tauxzp, 'bo')
+
+		plt.plot(sigyyp2, tauyzp, 'go')
+		plt.plot(sigzzp2, -tauyzp, 'go')
+
+	plt.gca().set_aspect('equal', adjustable='box')
+	plt.draw()
+	plt.show()
+
 
 def plot(sigxx, sigyy, tauxy):
 	plt.hold(True)
