83e3cbd549a28bd8ccc14c66805795f4c8095b55
liani <liani@lychee.(none)>
2015-04-26 19:47:45 -0400

    correctly plotting 3d state of stress

--- mohr.py
+++ mohr.py
@@ -3,6 +3,8 @@ import math
 import numpy as np
 
 
+# PLOTTING 2D STATE OF STRESS
+
 def convert(sigxx, sigyy, tauxy, theta):
     ''' Calculates principal stresses for 2D state of stress. '''
 
@@ -13,6 +15,22 @@ def convert(sigxx, sigyy, tauxy, theta):
 
     return [sigxxp, sigyyp, tauxyp]
 
+
+def plot2d(sigxx, sigyy, tauxy):
+    ''' Plots the 2D state of stress. '''
+
+    plt.hold(True)
+    for i in range(360):
+        [sigxxp, sigyyp, tauxyp] = convert(sigxx, sigyy, tauxy, i)
+        plt.plot(sigxxp, tauxyp, 'ro')
+        plt.plot(sigyyp, -tauxyp, 'bo')
+
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.draw()
+    plt.show()
+
+
+# PLOTTING 3D STATE OF STRESS
 
 def calc_sigp(sigxx, sigyy, sigzz, tauxy, tauxz, tauyz):
     ''' Calculates principal stresses for 3D state of stress. '''
@@ -33,28 +51,31 @@ def find_center(sigp1, sigp2):
     return (sigp1+sigp2)/2
 
 
-def find_radius():
+def find_radius(sigp, center):
     ''' Find the radius of a circle. '''
 
+    return (sigp - center)
 
-def plot(sigxx, sigyy, tauxy):
-    ''' Plots the 2D state of stress. '''
 
-    plt.hold(True)
-    for i in range(360):
-        [sigxxp, sigyyp, tauxyp] = convert(sigxx, sigyy, tauxy, i)
-        plt.plot(sigxxp, tauxyp, 'ro')
-        plt.plot(sigyyp, -tauxyp, 'bo')
+def calc_circle(center, radius, theta):
+    ''' Calculates circle path given center and radius. '''
 
-    plt.gca().set_aspect('equal', adjustable='box')
-    plt.draw()
-    plt.show()
+    radian = math.radians(theta)
+    x = radius * math.cos(radian) + center
+    y = radius * math.sin(radian)
 
+    return [x, y]
+
+
+# def define_circle(sigp1, sigp2):
+#     ''' Calculates circle center and radius. '''
+
+#     center = (sigp1 + sigp2)/2
+
+#     return [center]
 
 def plot3d(sigxx, sigyy, sigzz, tauxy, tauxz, tauyz):
     ''' Plots the 3D state of stress. '''
-
-    # plot(sigxx, sigyy, tauxy)
 
     [sigxxp, sigyyp, sigzzp] = calc_sigp(sigxx, sigyy, sigzz, tauxy, tauxz, tauyz)
 
@@ -62,26 +83,26 @@ def plot3d(sigxx, sigyy, sigzz, tauxy, tauxz, tauyz):
     cxz = find_center(sigxxp, sigzzp)
     cyz = find_center(sigyyp, sigzzp)
 
-    # return cxy
+    rxy = find_radius(sigxxp, cxy)
+    rxz = find_radius(sigxxp, cxz)
+    ryz = find_radius(sigyyp, cyz)
 
-    return [cxy, cxz, cyz]
-    # cxz
-    # cyz
+    # 
+    # [cxy, rxy] = define_circle(sigxxp, sigyyp)
+    # [cxz, rxz] = define_circle(sigxxp, sigzzp)
+    # [cyz, ryz] = define_circle(sigyyp, sigzzp)
+    # 
 
-    # plt.hold(True)
-    # for i in range(360):
-    #     [sigxxp1, sigyyp1, tauxyp] = convert(sigxx, sigyy, tauxy, i)
-    #     [xval1, yval1] = convert_other(sigxxp1, sigxx, sigzz, i)
-    #     [xval2 , yval2] = convert_other(sigyyp1, sigyy, sigzz, i)
+    plt.hold(True)
+    for i in range(360):
+        [xval1, yval1] = calc_circle(cxy, rxy, i)
+        [xval2, yval2] = calc_circle(cxz, rxz, i)
+        [xval3, yval3] = calc_circle(cyz, ryz, i)
 
-    #     plt.plot(sigxxp1, tauxyp, 'ro')
-    #     plt.plot(sigyyp1, -tauxyp, 'ro')
+        plt.plot(xval1, yval1, 'go')
+        plt.plot(xval2, yval2, 'bo')
+        plt.plot(xval3, yval3, 'ro')
 
-    #     plt.plot(xval1, yval1, 'go')
-
-    #     plt.plot(xval2, yval2, 'bo')
-
-    # print sigxxp1
-    # plt.gca().set_aspect('equal', adjustable='box')
-    # plt.draw()
-    # plt.show()
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.draw()
+    plt.show()
